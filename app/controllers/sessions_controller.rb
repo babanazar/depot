f1b0dfb85535bd7609ba5b62cec13d96ab3cda03
babanazar <g.babanazar@gmail.com>
2020-06-04 10:34:50 +0300

    can't delete last user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,16 @@
 class SessionsController < ApplicationController
   skip_before_action :authorize
   def new
-    user = User.find_by(name: params[:name])
-    if user.try(:authenticate, params[:password])
-      session[:user_id] = user.id
-      redirect_to admin_index_url
-    else
-      redirect_to login_url, alert: "Invalid user/password combination"
-    end
   end
 
   def create
+    user = User.find_by(name: params[:name])
+    if user.try(:authenticate, params[:password])
+      session[:user_id] = user.id
+      redirect_to admin_url
+    else
+      redirect_to login_url, alert: "Invalid user/password combination"
+    end
   end
 
   def destroy
